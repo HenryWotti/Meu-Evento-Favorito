d3854ff943790cc1448af16b069ff6bf77220fcd
JuliaCald <j.caldeira.j@gmail.com>
2022-07-21 17:20:52 -0300

    Criação de evento com css

--- frontend/src/pages/createEvent.tsx
+++ frontend/src/pages/createEvent.tsx
@@ -6,69 +6,115 @@ export default function CreateEventPage() {
   return (
     <>
       <Head>
-        <title className={Styles.sectionTitle}>CRIAR EVENTO</title>
+        <title>CRIAR EVENTO</title>
       </Head>
+      
       <main>
-        <section>
-            <h1>Informações do criador do evento</h1>
-          <form>
-            <label>
-                Nome
-                <input type="text" placeholder="Nome"/>
-            </label>
-            <label>
-                E-mail
-                <input type="email" placeholder="Seu e-mail favorito"/>
-            </label>
+        <section className={Styles.initialSection}>
 
-            <h1>Informações do evento</h1>
-            <label>
-                Nome do evento
-                <input type="text" placeholder="Nome"/>
-            </label>
-            <label>
-                Descrição do evento
-                <input type="text" placeholder="Capriche e torne seu evento atrativo!"/>
-            </label>
-            <label>
-                Quantidade máxima de pessoas
-                <input type="number"/>
-            </label>
-            <legend>Público-alvo</legend>
-            <label>
-                <input type="checkbox" name="publicoalvo"/>
-                Homens 
-            </label>
-            <label>
-                <input type="checkbox" name="publicoalvo"/>
-                Mulheres 
-            </label>
-            <label>
-                <input type="checkbox" name="publicoalvo"/>
-                Crianças 
-            </label>
-            
-            <input type="submit" value="Enviar" />
-          </form>
+            <section  className={Styles.sectionTitle}>
+                <a target="_blank" href="" rel="noopener noreferrer">
+                            <img src="./images/goBack.svg" alt="voltar" />
+                </a>
+                <h1 >CRIAÇÃO DE EVENTO</h1>
+            </section>
 
-          <label>
-                Idade mínima requerida
-                <input type="number"/>
-          </label>
+            <section className={Styles.sectionForm}>
+                <div className={Styles.dotAndTitle}>
+                    <img src="./images/pontoAmarelo.svg" alt="ponto amarelo"/>
+                    <h2>Informações do criador do evento</h2>
+                </div>
+            <form>
+                <div className={Styles.infoCreator}>
+                    <div className={Styles.labelAndInput}>
+                        <label>
+                            Nome<br/>
+                            <input className={Styles.inputSmall} type="text" placeholder="Nome"/>
+                        </label>
+                    </div>
+                    <div className={Styles.labelAndInput}>
+                        <label>
+                            E-mail<br/>
+                            <input type="email" placeholder="Seu e-mail favorito"/>
+                        </label>
+                    </div>
+                </div>
+
+                <div className={Styles.dotAndTitle}>
+                    <img src="./images/pontoAmarelo.svg" alt="ponto amarelo"/>
+                    <h2>Informações do evento</h2>
+                </div>
+                <div className={Styles.labelAndInput}>
+                    <label>
+                        Nome do evento<br/>
+                        <input type="text" placeholder="Nome"/>
+                    </label>
+                </div>
+
+                <div className={Styles.labelAndInput}>
+                    <label>
+                        Descrição do evento<br/>
+                        <input type="text" placeholder="Capriche e torne seu evento atrativo!"/>
+                    </label>
+                </div>
+                <div className={Styles.labelAndInput}>
+                    <label>
+                        Quantidade máxima de pessoas<br/>
+                        <input type="number"/>
+                    </label>
+
+                    <div className={Styles.checkboxes}>
+                    <legend>Público-alvo<br/></legend>
+                    <div>
+                        <label>
+                            <input type="checkbox" name="publicoalvo"/>
+                            Homens 
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            <input type="checkbox" name="publicoalvo"/>
+                            Mulheres 
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            <input type="checkbox" name="publicoalvo"/>
+                            Crianças 
+                        </label>
+                    </div>
+                        
+                    </div>
+
+                </div>
             
-            <label>
-                Início do evento
-                <input type="datetime-local"/>
-            </label>
-            <label>
-                Fim do evento   
-                <input type="datetime-local"/>
-            </label>
-            <label>
-                Local do evento
-                <input type="text"/>
-            </label>
+                <label>
+                        Idade mínima requerida<br/>
+                        <input type="number"/>
+                </label> <br/>
+                
+                <label>
+                    Início do evento<br/>
+                    <input type="datetime-local"/>
+                </label> <br/>
+                <label>
+                    Fim do evento   <br/>
+                    <input type="datetime-local"/>
+                </label> <br/>
+                <label>
+                    Local do evento<br/>
+                    <input type="text"/>
+                </label>
+
+                <br/>
+
+
+                <input type="submit" value="Enviar" />
+            </form>
+
+            </section>
         </section>
+        
       </main>
 
     </>
