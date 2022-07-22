66681c796656d5ed5e0f698effb531bf290e7828
JuliaCald <j.caldeira.j@gmail.com>
2022-07-21 21:03:17 -0300

    Finalização do scss da página de criação de evento + upload de imagem adicionado

--- frontend/src/pages/createEvent.tsx
+++ frontend/src/pages/createEvent.tsx
@@ -29,13 +29,13 @@ export default function CreateEventPage() {
                     <div className={Styles.labelAndInput}>
                         <label>
                             Nome<br/>
-                            <input className={Styles.inputSmall} type="text" placeholder="Nome"/>
+                            <input className={Styles.inputMedium} type="text" placeholder="Nome"/>
                         </label>
                     </div>
                     <div className={Styles.labelAndInput}>
                         <label>
                             E-mail<br/>
-                            <input type="email" placeholder="Seu e-mail favorito"/>
+                            <input className={Styles.inputMedium} type="email" placeholder="Seu e-mail favorito"/>
                         </label>
                     </div>
                 </div>
@@ -44,72 +44,98 @@ export default function CreateEventPage() {
                     <img src="./images/pontoAmarelo.svg" alt="ponto amarelo"/>
                     <h2>Informações do evento</h2>
                 </div>
+
                 <div className={Styles.labelAndInput}>
                     <label>
                         Nome do evento<br/>
-                        <input type="text" placeholder="Nome"/>
+                        <input className={Styles.inputMedium} type="text" placeholder="Nome"/>
                     </label>
                 </div>
 
                 <div className={Styles.labelAndInput}>
                     <label>
                         Descrição do evento<br/>
-                        <input type="text" placeholder="Capriche e torne seu evento atrativo!"/>
+                        <input className={Styles.inputLarge} type="text" placeholder="Capriche e torne seu evento atrativo!"/>
                     </label>
                 </div>
+
                 <div className={Styles.labelAndInput}>
                     <label>
                         Quantidade máxima de pessoas<br/>
-                        <input type="number"/>
+                        <input className={Styles.inputSmall} type="number"/>
                     </label>
-
+                </div>
+                <div className={Styles.labelAndInput}>
                     <div className={Styles.checkboxes}>
-                    <legend>Público-alvo<br/></legend>
-                    <div>
-                        <label>
-                            <input type="checkbox" name="publicoalvo"/>
-                            Homens 
-                        </label>
-                    </div>
-                    <div>
-                        <label>
-                            <input type="checkbox" name="publicoalvo"/>
-                            Mulheres 
-                        </label>
-                    </div>
-                    <div>
-                        <label>
-                            <input type="checkbox" name="publicoalvo"/>
-                            Crianças 
-                        </label>
-                    </div>
-                        
+                        <legend>Público-alvo<br/></legend>
+                        <div>
+                            <label>
+                                <input type="checkbox" name="publicoalvo"/>
+                                Homens 
+                            </label>
+                        </div>
+                        <div>
+                            <label>
+                                <input type="checkbox" name="publicoalvo"/>
+                                Mulheres 
+                            </label>
+                        </div>
+                        <div>
+                            <label>
+                                <input type="checkbox" name="publicoalvo"/>
+                                Crianças 
+                            </label>
+                        </div>
+                        <br/>
                     </div>
 
                 </div>
             
-                <label>
+                <label className={Styles.labelAndInput}>
                         Idade mínima requerida<br/>
-                        <input type="number"/>
-                </label> <br/>
-                
-                <label>
+                        <input className={Styles.inputSmall} type="number"/>
+                        <br/>
+                </label> 
+                <br/>
+                <label className={Styles.labelAndInput}>
                     Início do evento<br/>
-                    <input type="datetime-local"/>
-                </label> <br/>
-                <label>
-                    Fim do evento   <br/>
-                    <input type="datetime-local"/>
-                </label> <br/>
-                <label>
+                    <input className={Styles.inputSmall} type="datetime-local"/>
+                    <br/>
+                </label> 
+                <br/>
+                <label className={Styles.labelAndInput}>
+                    Fim do evento <br/>
+                    <input className={Styles.inputSmall} type="datetime-local"/>
+                    <br/>
+                </label> 
+                <br/>
+                <label className={Styles.labelAndInput}>
                     Local do evento<br/>
-                    <input type="text"/>
+                    <input className={Styles.inputMedium} type="text"/>
+                    <br/>
                 </label>
-
                 <br/>
 
+                <div className={Styles.loadImage}>
+                    <img src="./images/uparArquivo.svg" alt="carregar arquivo ícone"/>
+                    <label className={Styles.labelAndInput}>
+                        Carregar foto do evento <br/>
+                        <input type="file" />
+                    </label>                    
+                </div>
 
-                <input type="submit" value="Enviar" />
+                <div className={Styles.sendSection}>
+                    <div>
+
+                    </div>
+                    <div>
+                        <img src="./images/trioMinions.png" alt="três minions"/>
+                    </div>
+                    <div>
+                        <input className={Styles.sendButton} type="submit" value="Salvar" />
+                    </div>
+                    
+                </div>
             </form>
 
             </section>
